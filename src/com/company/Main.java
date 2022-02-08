51101d6e301883da6045c5e43af9603b64623255
Dalton Price <dhp0015@auburn.edu>
2022-02-07 23:05:33 -0600

    implemented functional music player and updated UI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,25 @@
 package com.company;
 
+// Java program to play an Audio
+// file using Clip Object
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-	new MusicPlayer();
-
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        new MusicPlayer();
     }
+
+
+
+
 }
+
--- src/com/company/MusicPlayer.java
+++ src/com/company/MusicPlayer.java
@@ -1,9 +1,12 @@
 package com.company;
 import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,21 +29,64 @@ public class MusicPlayer {
     private JLabel pauseLabel;
     private JPanel pausePanel;
     private JPanel rightPanel;
+    private JSlider songSlider;
+    private JPanel loopPanel;
+    private JLabel loopLabel;
+    private JPanel sliderPanel;
+
+    // to store current position
+    Long currentFrame;
+    Clip clip;
+
+    // current status of clip
+    String status;
+
+    AudioInputStream audioInputStream;
+    static String filePath;
+
+
+
+
 
     /**
      * Constructor for Music Player
      * @throws IOException
      */
-    public MusicPlayer() throws IOException {
+    public MusicPlayer() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
         JFrame frame = new JFrame("Discover a new song daily");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(750, 550);
+        frame.setSize(750, 450);
+        frame.setResizable(false);
+
+        filePath = "C:\\Users\\Dalton Price\\Downloads\\RapSongOfTheDayApp\\Kodak_Versatile3.wav";
+
+        // create AudioInputStream object
+        audioInputStream =
+                AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+
+        // create clip reference
+        clip = AudioSystem.getClip();
+
+        // open audioInputStream to the clip
+        clip.open(audioInputStream);
+
+        //clip.loop(Clip.LOOP_CONTINUOUSLY);
 
         //Set images for buttons
         Image myImage = ImageIO.read(getClass().getResource("downloadplay.png"));
-        myImage = myImage.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+        myImage = myImage.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
         ImageIcon myImageIcon = new ImageIcon(myImage);
         playButtonLabel.setIcon(myImageIcon);
+
+        Image pauseImage = ImageIO.read(getClass().getResource("images.png"));
+        pauseImage = pauseImage.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
+        ImageIcon pauseImageIcon = new ImageIcon(pauseImage);
+        pauseLabel.setIcon(pauseImageIcon);
+
+        Image loopImage = ImageIO.read(getClass().getResource("loop.png"));
+        loopImage = loopImage.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
+        ImageIcon loopImageIcon = new ImageIcon(loopImage);
+        loopLabel.setIcon(loopImageIcon);
 
         //Display current date
         displayCurrentDate();
@@ -56,43 +102,55 @@ public class MusicPlayer {
             public void mouseClicked(MouseEvent e) {
 
                 super.mouseClicked(e);
-                System.out.println("Hello");
-                int clicked = 0;
-                clicked++;
-                //play the musuci and switch sign
+                play();
+            }
+        });
 
-                Image pauseImage = null;
-                try {
-                    pauseImage = ImageIO.read(getClass().getResource("images.png"));
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                pauseImage = pauseImage.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
-                ImageIcon pauseImageIcon = new ImageIcon(pauseImage);
-                playButtonLabel.setIcon(pauseImageIcon);
 
-                if (clicked % 2 == 0) {
-                    playButtonLabel.setIcon(myImageIcon);
-                }
+        loopLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
 
+        pauseLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                pause();
             }
         });
     }
 
-
+    //END CONSTUCTOR START METHODS
 
     //Display the current date for the date label
     public void displayCurrentDate() {
-        //dateLabel.setText(String.valueOf(LocalDate.now()));
 
-        String pattern = "dd MM yyyy";
         String p = "EEEEE, MMMMM dd, yyyy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(p);
 
         String date = simpleDateFormat.format(new Date());
         dateLabel.setText(date);
-
     }
 
+    // Method to play the audio
+    public void play() {
+        //start the clip
+        clip.start();
+        status = "play";
+    }
+
+    // Method to pause the audio
+    public void pause() {
+        if (status.equals("paused")) {
+            return;
+        }
+        this.currentFrame =
+                this.clip.getMicrosecondPosition();
+        clip.stop();
+        status = "paused";
+    }
 
 }
