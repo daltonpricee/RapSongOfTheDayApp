1ed566e0fa94ef0e02e6d166d00cfffd85d84160
Dalton Price <dhp0015@auburn.edu>
2022-02-17 01:04:53 -0600

    updated db to allow more songs and function music player for all songs selected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,14 @@
 package com.company;
-
-// Java program to play an Audio
-// file using Clip Object
-import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import java.sql.SQLException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * Main method to call instance of MusicPlayer
+ */
 public class Main {
-
-    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, SQLException {
         new MusicPlayer();
     }
 
--- src/com/company/MusicPlayer.java
+++ src/com/company/MusicPlayer.java
@@ -3,14 +3,19 @@ import javax.imageio.ImageIO;
 import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * MusicPlauer class to control the music player and GUI components.
+ */
 public class MusicPlayer {
     private JPanel mainPanel;
     private JLabel rightSideTopLabel;
@@ -33,53 +38,89 @@ public class MusicPlayer {
     private JPanel loopPanel;
     private JLabel loopLabel;
     private JPanel sliderPanel;
+    private JLabel songSubLabel;
+    private JLabel rightHeader;
+    private Statement myStmt = null;
+    private Connection myConn = null;
+    private ResultSet myRs = null;
+    private String url;
+    private String name;
+    private String title;
+    private String year;
+    private String album;
+    private String title2;
 
-    // to store current position
+    //store current position
     Long currentFrame;
     Clip clip;
 
-    // current status of clip
+    //current status of clip
     String status;
 
+    //initialize stream obj and filepath
     AudioInputStream audioInputStream;
     static String filePath;
 
-
-
-
-
     /**
-     * Constructor for Music Player
+     * Constructor for MusicPlayer
      * @throws IOException
      */
-    public MusicPlayer() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+    public MusicPlayer() throws IOException, UnsupportedAudioFileException, LineUnavailableException, SQLException {
         JFrame frame = new JFrame("Discover a new song daily");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(750, 450);
+        frame.setSize(925, 650);
         frame.setResizable(false);
 
-        filePath = "C:\\Users\\Dalton Price\\Downloads\\RapSongOfTheDayApp\\Kodak_Versatile3.wav";
+        //DB STUFF
+        Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/musicdb",
+                "student", "student");
 
-        // create AudioInputStream object
+        //Create query and execute for hip hop radio.
+        String query3 = "SELECT * FROM song_info ORDER BY RAND() LIMIT 1";
+        myStmt = myConn.createStatement();
+        myRs = myStmt.executeQuery(query3);
+
+        //See if RS returns anything and get info based on it.
+        while (myRs.next()) {
+
+            System.out.println(myRs.getString(2));
+            name = myRs.getString(2);
+
+            System.out.println(myRs.getString(4));
+            album = myRs.getString(4);
+
+            System.out.println(myRs.getString(5));
+            title = myRs.getString(5);
+            title2 = title.replaceAll("\\s", "");
+
+            System.out.println(myRs.getString(8));
+            year = myRs.getString(8);
+
+
+            //the filepath string for a specific song
+            filePath = "C:\\Users\\Dalton Price\\Downloads\\RapSongOfTheDayApp\\" + title2 + ".wav";
+            songNameLabel.setText(name + " - " + title);
+            songSubLabel.setText(album + " - " + year);
+        }
+
+        //create AudioInputStream object
         audioInputStream =
                 AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
 
-        // create clip reference
+        //create clip reference
         clip = AudioSystem.getClip();
 
-        // open audioInputStream to the clip
+        //open audioInputStream to the clip
         clip.open(audioInputStream);
 
-        //clip.loop(Clip.LOOP_CONTINUOUSLY);
-
-        //Set images for buttons
+        //Set images for labels
         Image myImage = ImageIO.read(getClass().getResource("downloadplay.png"));
-        myImage = myImage.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
+        myImage = myImage.getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH);
         ImageIcon myImageIcon = new ImageIcon(myImage);
         playButtonLabel.setIcon(myImageIcon);
 
         Image pauseImage = ImageIO.read(getClass().getResource("images.png"));
-        pauseImage = pauseImage.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
+        pauseImage = pauseImage.getScaledInstance(38, 38, java.awt.Image.SCALE_SMOOTH);
         ImageIcon pauseImageIcon = new ImageIcon(pauseImage);
         pauseLabel.setIcon(pauseImageIcon);
 
@@ -88,9 +129,15 @@ public class MusicPlayer {
         ImageIcon loopImageIcon = new ImageIcon(loopImage);
         loopLabel.setIcon(loopImageIcon);
 
+        rightHeader.setText("<HTML><U>C O R E     M U S I C</U></HTML>");
+
+        onThisDayInfoLabel.setText("<html><center>Kodak was sliding going crazy." +
+                "<br> This was a good album and elevated kodak alot with his music.</html>");
+
         //Display current date
         displayCurrentDate();
-        rightSideTopLabel.setText("<html><center>S O N G  <br><br> O F <br><br> T H E <br><br> D A Y</html>");
+        //rightSideTopLabel.setText("<html><center>S O N G  <br><br> O F <br><br> T H E <br><br> D A Y</html>");
+        rightSideTopLabel.setText("New music, everyday.");
         frame.setVisible(true);
         frame.add(mainPanel);
 
@@ -100,20 +147,25 @@ public class MusicPlayer {
         playButtonLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-
                 super.mouseClicked(e);
                 play();
             }
         });
 
-
+        /**
+         * Action listener for loop button label
+         */
         loopLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+                //clip.loop(Clip.LOOP_CONTINUOUSLY);
             }
         });
 
+        /**
+         * Action listener for pause button label
+         */
         pauseLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -123,26 +175,28 @@ public class MusicPlayer {
         });
     }
 
-    //END CONSTUCTOR START METHODS
-
-    //Display the current date for the date label
+    /**
+     * Display current date at top of screen
+     */
     public void displayCurrentDate() {
-
         String p = "EEEEE, MMMMM dd, yyyy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(p);
-
         String date = simpleDateFormat.format(new Date());
         dateLabel.setText(date);
     }
 
-    // Method to play the audio
+    /**
+     * Play the selected music
+     */
     public void play() {
         //start the clip
         clip.start();
         status = "play";
     }
 
-    // Method to pause the audio
+    /**
+     * Pause the song
+     */
     public void pause() {
         if (status.equals("paused")) {
             return;
